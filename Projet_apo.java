3bfcc38467e63cd3b3eac57f042107a1e65aa083
SamLams24 <attiogbesamuel310@gmail.com>
2023-11-02 05:00:51 +0100

    Premier depot du projet_apo_java

---
+++ Projet_apo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projet_apo;
+
+/**
+ *
+ * @author D e l l - E6430
+ */
+public class Projet_apo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
